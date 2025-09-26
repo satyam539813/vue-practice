30f79bf59a8bfa1f095f9465aec9a4dd55a5dff6
satyam539813 <116088612+satyam539813@users.noreply.github.com>
2025-09-26 20:33:24 +0530

    committed

--- src/router.js
+++
@@ -1,9 +0,0 @@
-import {createMemoryHistory} from "vue-router";
-import {Home} from "lucide-vue-next";
-
-const routes=[{path:"/",component:Home}];
-
-(
-    {history:createMemoryHistory(),
-        routes}
-);
