928274dc41eea2a68badcac0ee8c8c90e5ce8b8f
satyam539813 <116088612+satyam539813@users.noreply.github.com>
2025-12-12 00:15:32 +0530

    changes done

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import PrimeVue from 'primevue/config';
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app=createApp(App)
-     app.use(PrimeVue);
-     app.mount('#app')
